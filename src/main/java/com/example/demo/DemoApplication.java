4c81ddb339356af1aab900b9f314b535d6bdd72b
unknown <dangtruong3122005@gmail.com>
2025-05-27 20:33:20 +0700

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        String test = this.userService.handleHello();
+        return "hello";
+    }
+}
+
+// @RestController
+// public class UserController {
+
+// //DI : Dependency Injection
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("/")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
